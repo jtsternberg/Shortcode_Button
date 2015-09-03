29ce867b625635ed893f918faedff84911664d27
Jay Wood <jerry@webdevstudios.com>
2015-09-03 11:18:47 -0400

    Update version

--- shortcode-button.php
+++ shortcode-button.php
@@ -3,7 +3,7 @@ if ( ! class_exists( '_Shortcode_Button_' ) ) :
 /**
  * Tinymce and Quicktag buttons for outputting shortcodes
  *
- * @version 0.1.0
+ * @version 0.1.1
  */
 class _Shortcode_Button_ {
 
